24624a26d3961c463b1e3ee5da0f560806b7c2b6
Bue Petersen <bue@praqma.net>
2013-01-29 17:36:02 +0100

    added new test of model2 to see if coverage drops

---
+++ src/test/java/net/praqma/roadshow/TestCalculatorModel2.java
@@ -0,0 +1,12 @@
+package net.praqma.roadshow;
+
+import net.praqma.roadshow.model.CalculatorModel;
+import net.praqma.roadshow.model.CalculatorModelImpl;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestCalculatorModel2 {
+	
+		
+}
