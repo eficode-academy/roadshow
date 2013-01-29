e098ab79a6665c057a7b65b3e8b38fde0b02cabb
Mads Nielsen <inkspot84@gmail.com>
2013-01-29 10:53:53 +0100

    Operands check

--- src/main/java/net/praqma/roadshow/model/CalculatorModelImpl.java
+++ src/main/java/net/praqma/roadshow/model/CalculatorModelImpl.java
@@ -1,45 +1,103 @@
 package net.praqma.roadshow.model;
 
 public class CalculatorModelImpl implements CalculatorModel {
-
+    
+    
+    private Number storeValue;
+    
 	@Override
 	public Number add(Number... numbers) {
-		double res = 0d; 
-		
-		for(Number number : numbers) {
-			res+=number.doubleValue();
-		}
+		double res = 0d;
+        if(!checkOperands(numbers)) { 
+            return res;
+        } else {
+            for(Number number : numbers) {
+                res+=number.doubleValue();
+            }
+        }
 		return res;
 	}
 
 	@Override
 	public Number subtract(Number... numbers) {
-		// TODO Auto-generated method stub
-		return null;
+        double res = 0d;
+		if(!checkOperands(numbers)) { 
+            return res;
+        } else {            
+            for(int i=0; i<numbers.length; i++) {                
+                if(i == 0) {
+                    res = numbers[i].doubleValue();
+                } else {
+                    res -= numbers[i].doubleValue();
+                }
+               
+            }
+        }
+		return res;
 	}
 
 	@Override
 	public Number multiply(Number... numbers) {
-		// TODO Auto-generated method stub
-		return null;
+        double res = 0d;
+		if(!checkOperands(numbers)) { 
+            return res;
+        } else {            
+            for(int i=0; i<numbers.length; i++) {                
+                if(i == 0) {
+                    res = numbers[i].doubleValue();
+                } else {
+                    res *= numbers[i].doubleValue();
+                }               
+            }
+        }
+		return res;
 	}
 
 	@Override
 	public Number divide(Number... numbers) {
-		// TODO Auto-generated method stub
-		return null;
+        double res = 0d;
+		if(!checkOperands(numbers)) { 
+            return res;
+        } else {            
+            for(int i=0; i<numbers.length; i++) {
+                if(numbers[i].doubleValue() == 0d) {
+                    return 0d;
+                }                
+                if(i == 0) {
+                    res = numbers[i].doubleValue();
+                } else {
+                    res /= numbers[i].doubleValue();
+                }
+                
+               
+            }
+        }
+		return res;
 	}
 
 	@Override
-	public void store(Number number) {
-		// TODO Auto-generated method stub
+	public void store(Number storeValue) {
+		this.storeValue = storeValue;
 		
 	}
 
 	@Override
 	public Number load() {
-		// TODO Auto-generated method stub
-		return null;
+        return this.storeValue;
 	}
+    
+    public boolean checkOperands(Number... number) {
+        if(number == null || number.length < 2) {
+            return false;
+        } else {
+            for(Number num : number) {
+                if(num == null) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
 	
 }
