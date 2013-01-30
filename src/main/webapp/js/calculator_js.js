1178f593e08a573c49a1bb08e394cc423a732631
Mads Nielsen <inkspot84@gmail.com>
2013-01-30 16:01:31 +0100

    Added file

---
+++ src/main/webapp/js/calculator_js.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.onkeypress = function(e) {
+    e = e || window.event;
+    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+    if (charCode) {
+        //alert("Character typed: " + String.fromCharCode(charCode));
+        
+    }
+};
