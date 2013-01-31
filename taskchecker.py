c2edd4ca99a4e78856d33bb05f7238019efe4eaa
Andrius <Andrius.or@gmail.com>
2013-01-31 17:05:38 +0100

    Fixed taskchecker script

--- taskchecker.py
+++ taskchecker.py
@@ -4,7 +4,7 @@ import re
 pipe = Popen(["git", "log", "-1"], stdout=PIPE)
 gitLog = pipe.communicate()[0]
 
-matchObj = re.match(r"Task\s+(\d+)",gitLog, flags=0)
+matchObj = re.search(r"Task\s+(\d+)",gitLog, flags=0)
 
 if not matchObj:
     call("echo 'Your commit message needs to contain Task XXXX where X is number.'",shell=1)
