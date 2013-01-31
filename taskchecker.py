d3c7e555cf27441d177100a119c4d6b88614fbfa
Andrius <Andrius.or@gmail.com>
2013-01-31 16:48:03 +0100

    Task 1 updated the error message

--- taskchecker.py
+++ taskchecker.py
@@ -7,5 +7,5 @@ gitLog = pipe.communicate()[0]
 matchObj = re.match(r"Task\s+(\d+)",gitLog, flags=0)
 
 if not matchObj:
-    call("echo 'You commit needs to contain Task XXXX where X is number .'",shell=1)
+    call("echo 'Your commit message needs to contain Task XXXX where X is number.'",shell=1)
     call(["exit","1"], shell=1) 
