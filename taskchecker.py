794d809d799ab89d57f92d4cc49837903e298cab
Andrius <Andrius.or@gmail.com>
2013-01-31 16:35:40 +0100

    Created a python script that parses the git log for a task spesification.

---
+++ taskchecker.py
@@ -0,0 +1,11 @@
+from subprocess import Popen, PIPE, call
+import re
+
+pipe = Popen(["git", "log", "-1"], stdout=PIPE)
+gitLog = pipe.communicate()[0]
+
+matchObj = re.match(r"Task\s+(\d+)",gitLog, flags=0)
+
+if not matchObj:
+    call("echo 'You need to spesify a task.'",shell=1)
+    call(["exit","1"], shell=1) 
