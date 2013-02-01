b914ca776c12b097a768656fcd2fffab10dbbf56
Bue Petersen <bue@praqma.net>
2013-02-01 01:41:42 +0100

    changed selenium port for functional test

--- src/test/java/net/praqma/roadshow/functional/TestAdditionRC.java
+++ src/test/java/net/praqma/roadshow/functional/TestAdditionRC.java
@@ -12,7 +12,7 @@ public class TestAdditionRC {
 
 	@Before
 	public void setUp() throws Exception {        
-		selenium = new DefaultSelenium("roadshowseleniumserver", 4444, "*firefox", "http://roadshowtomcatserver:8080/");
+		selenium = new DefaultSelenium("roadshowseleniumserver", 44444, "*firefox", "http://roadshowtomcatserver:8080/");
 		selenium.start();
 	}
 
