ae20ecda060c05c796289083c671506bf8540127
Jan Krag <jankrag@gmail.com>
2014-10-24 17:01:03 +0200

    Added fixme and removed serialUID

--- src/main/java/net/praqma/roadshow/Calculator.java
+++ src/main/java/net/praqma/roadshow/Calculator.java
@@ -17,7 +17,8 @@ import net.praqma.roadshow.model.CalculatorModelImpl;
 @WebServlet(urlPatterns = {"/Calculator"}, description = "This is a small Calculator App used for our roadshow", displayName = "Calulator Demo", name = "Calculator")
 public class Calculator extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    //FIXME
+    //private static final long serialVersionUID = 1L;
     private static final CalculatorModel model = new CalculatorModelImpl();
 
     public Calculator() {
